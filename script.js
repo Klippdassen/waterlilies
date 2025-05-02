1c50dd629c7e651fda2aa6912aff9f2c6c95a2ae
Klippdassen <spork.nordin@gmail.com>
2025-05-02 21:31:02 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const lilyCount = 15;
+const container = document.querySelector('.lilies-container');
+
+for (let i = 0; i < lilyCount; i++) {
+  const lily = document.createElement('div');
+  lily.classList.add('lily');
+
+  // Random start position
+  lily.style.top = Math.random() * window.innerHeight + 'px';
+  lily.style.left = Math.random() * window.innerWidth + 'px';
+
+  // Optional: Different animation delay
+  lily.style.animationDelay = `${Math.random() * 5}s`;
+
+  container.appendChild(lily);
+}
